8f2b84eb86209e5ca654cf467153870a2408972a
Richard Mands <richard@eduku.org>
2017-03-23 21:34:19 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+$ = jQuery = require('jquery');
+var REact = require('react');
+var Home = require('./components/HomePage')
+
+var App = console.log('Hello world from Browserify');
+
+module.exports = App;
