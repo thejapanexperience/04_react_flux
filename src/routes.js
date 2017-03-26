9ff6f8e0dcb8fe17504bca7db6cdd95d233d27e6
Richard Mands <richard@eduku.org>
2017-03-26 20:01:32 +0800

    update

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+
+var routes = (
+  <Route name="app" path="/" handler={require('./components/app')}>
+    <DefaultRoute handler={require ('./components/homePage')} />
+    <Route name="authors" handler={require ('./components/authorPage')} />
+    <Route name="authors" handler={require ('./components/aboutPage')} />
+  </Route>
+)
