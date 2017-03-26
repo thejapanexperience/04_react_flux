420cb6ece5ba2bb3ab42075d9bff159e125b13f1
Richard Mands <richard@eduku.org>
2017-03-26 21:30:33 +0800

    update

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49159,16 +49159,21 @@ var React = require('react');
 
 var About = React.createClass({displayName: "About",
 
-  static: {
+  statics: {
     willTransitionTo: function(transition, params, query, callback){
       if(!confirm('Are you sure you want to read a page that\'s this boring?')){
         transition.about();
       } else {
         callback();
       }
+    },
+    willTransitionFrom: function(transition, component){
+      if(!confirm('Are you sure you want to leave a page that\'s this exciting?')){
+        transition.about();
+      }
     }
   },
-  
+
   render: function () {
     return (
       React.createElement("div", null, 
--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -2,16 +2,21 @@ var React = require('react');
 
 var About = React.createClass({
 
-  static: {
+  statics: {
     willTransitionTo: function(transition, params, query, callback){
       if(!confirm('Are you sure you want to read a page that\'s this boring?')){
         transition.about();
       } else {
         callback();
       }
+    },
+    willTransitionFrom: function(transition, component){
+      if(!confirm('Are you sure you want to leave a page that\'s this exciting?')){
+        transition.about();
+      }
     }
   },
-  
+
   render: function () {
     return (
       <div>
