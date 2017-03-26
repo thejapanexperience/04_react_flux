69b424f8b4f11aa692f5f566c4d31d143706dfc1
Richard Mands <richard@eduku.org>
2017-03-26 16:34:23 +0800

    update

---
+++ src/components/common/header.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var Header = React.crateClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            <img src="images/pluralsight-logo.png" alt=""/>
+          </a>
+          <ul className="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about">About</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+})
