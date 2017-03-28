1af2c8ed32e315656a797abfe2a4601fe9aa0243
Richard Mands <richard@eduku.org>
2017-03-28 14:43:08 +0800

    update

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -4,7 +4,32 @@ var AuthorForm = React.createClass({
 
   render: function() {
     return(
+      <form>
+        <h1>Manage Author</h1>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          className="form-control"
+          placeholder="First Name"
+          ref="firstName"
+          value=""
+        />
+        <br />
 
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          className="form-control"
+          placeholder="Last Name"
+          ref="lastName"
+          value=""
+        />
+        <br />
+
+        <input type="submit" value="Save" className="btn btn-default"/>
+      </form>
     );
   }
 })
